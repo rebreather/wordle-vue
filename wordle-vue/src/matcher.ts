127d6163c2dcad2fd19b555ce94ed76eaf553054
rebreather <yds04116@gmail.com>
2022-07-17 17:19:15 +0900

    주석 변경

--- wordle-vue/src/matcher.ts
+++ wordle-vue/src/matcher.ts
@@ -17,13 +17,14 @@ export function matchWord (
     const length = query.length
     const ret = Array(length).fill('gray') as MatchResult[]
 
+    // 기본적으로 gray로 되어있음, 같은 알파벳을 만날 때 green으로 변경
     for(let i = 0; i < length; i++) {
         if(query[i] === answer[i]) {
             ret[i] = 'green'
         }
     }
 
-    //count non-matched answer characters
+    //count non-matched answer characters 틀린 알파벳 카운트
     const chCountMap = {} as {[key: string]:number}
     for(let i = 0; i < length; i++) {
         if(ret[i] === 'gray') {
